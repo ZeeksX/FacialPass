4efc57fed8e295d51fb95430e10db4f829e576fd
ZeeksX <ikinwotezekiel@gmail.com>
2025-01-29 00:40:15 +0100

    added files via upload

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+// src/App.jsx
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import Login from "./pages/Login";
+import { AuthProvider, useAuth } from './components/Auth';
+import SignUp from './pages/SignUp';
+import Onboarding from './pages/Onboarding';
+
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  return isAuthenticated ? children : <Navigate to="/" />;
+};
+
+const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false); 
+  const [isMobile, setIsMobile] = useState(false); 
+
+  const toggleSidebar = () => {
+    setSidebarOpen(prev => !prev);
+  };
+
+  // Method to check if the screen is mobile
+  const checkIfMobile = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  useEffect(() => {
+    checkIfMobile(); 
+    window.addEventListener('resize', checkIfMobile); 
+
+    return () => {
+      window.removeEventListener('resize', checkIfMobile); 
+    };
+  }, []);
+
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<ParticlesReact />} />
+          <Route path="/login" element={<Login isMobile={isMobile} />} />
+          <Route path="/dashboard" element={
+            // <ProtectedRoute>
+              <HomePage sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} isMobile={isMobile} />
+            // </ProtectedRoute>
+          } />
+          <Route path="/signup" element={
+            <SignUp sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} isMobile={isMobile} />
+          } />
+          <Route path="/onboarding" element={
+            <Onboarding sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} isMobile={isMobile} />
+          } />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
