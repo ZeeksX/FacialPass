48e3e457acdd5061c2bc41110e4f304cb8d5629c
Joelsen <155982256+Joelsen@users.noreply.github.com>
2025-02-28 01:20:03 +0000

    added sidebars to all pages

--- src/pages/SelectedCourse.jsx
+++ src/pages/SelectedCourse.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import StudentSidebar from '../components/sidebars/StudentSidebar';
 
 const SelectedCourse = () => {
   return (
-    <div>SelectedCourse</div>
-  )
+    <div className="flex flex-row min-h-screen w-full bg-gray-100 text-[#0061A2]">
+      <StudentSidebar />
+      <div className="flex flex-col max-ml-60 ml-[20%] w-full p-6"></div>
+    </div>
+  );
 }
 
 export default SelectedCourse
