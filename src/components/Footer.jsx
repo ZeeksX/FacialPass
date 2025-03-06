fe18733c6a264e2ffe8bf36372758d89bba63589
ZeeksX <ikinwotezekiel@gmail.com>
2025-03-06 20:54:40 +0100

    fix: how it works section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,76 +13,79 @@ const Footer = () => {
     const company = ["About Us", "Careers", "Privacy Policy", "Terms & Conditions"];
     const resources = ["FAQs", "Blog", "Help Center"];
     const contact = [
-        { icon: <PhoneAndroidIcon />, feature: "+2349064048598" },
-        { icon: <MailOutlineIcon />, feature: "zeeksacademy@gmail.com" },
-        { icon: <LocationCityIcon />, feature: "131 Zeeks Avenue, Orlando, Flourida, United States" },
-    ]
+        { icon: <PhoneAndroidIcon />, feature: "+2349064048598", link: "tel:+2349064048598" },
+        { icon: <MailOutlineIcon />, feature: "zeeksacademy@gmail.com", link: "mailto:zeeksacademy@gmail.com" },
+        { icon: <LocationCityIcon />, feature: "131 Zeeks Avenue, Florida, United States", link: "https://www.google.com/maps/search/?api=1&query=131+Zeeks+Avenue,+Florida,+United+States" },
+    ];
     const socials = [
         <InstagramIcon />, <LinkedInIcon />, <FacebookIcon />, <YouTubeIcon />, <XIcon />
-    ]
+    ];
+
     return (
-        <>
-            <div className="flex flex-col text-white bg-[#0061A2] w-full pt-4 px-4 md:px-12">
-                <div className="flex flex-col lg:flex-row justify-between">
-                    {/* Logo Section */}
-                    <div className="flex flex-row items-center h-20">
-                        <img className="w-16 md:w-20" src={footerLogo} alt="FacialPass-logo" />
-                        <h3 className="text-2xl md:text-3xl font-semibold">FacialPass</h3>
-                    </div>
+        <div className="flex flex-col text-white bg-[#0061A2] w-full pt-4 px-4 md:px-12">
+            <div className="flex flex-col lg:flex-row justify-between">
+                {/* Logo Section */}
+                <div className="flex flex-row items-center h-20">
+                    <img className="w-16 md:w-20" src={footerLogo} alt="FacialPass-logo" />
+                    <h3 className="text-2xl md:text-3xl font-semibold">FacialPass</h3>
+                </div>
 
-                    {/* Company Section */}
-                    <div className="flex flex-col gap-2 mt-6">
-                        <h3 className="text-lg md:text-xl font-bold">Company</h3>
-                        {company.map((item, index) => (
-                            <h3 className="text-base md:text-lg cursor-pointer" key={index}>
-                                {item}
-                            </h3>
-                        ))}
-                    </div>
+                {/* Company Section */}
+                <div className="flex flex-col gap-2 mt-6">
+                    <h3 className="text-lg md:text-xl font-bold">Company</h3>
+                    {company.map((item, index) => (
+                        <h3 className="text-base md:text-lg cursor-pointer" key={index}>
+                            {item}
+                        </h3>
+                    ))}
+                </div>
 
-                    {/*Resources */}
-                    <div className="flex flex-col gap-2 mt-6">
-                        <h3 className="text-lg md:text-xl font-bold">Resources</h3>
-                        {resources.map((item, index) => (
-                            <h3 className="text-base md:text-lg cursor-pointer" key={index}>
-                                {item}
-                            </h3>
-                        ))}
-                    </div>
+                {/* Resources */}
+                <div className="flex flex-col gap-2 mt-6">
+                    <h3 className="text-lg md:text-xl font-bold">Resources</h3>
+                    {resources.map((item, index) => (
+                        <h3 className="text-base md:text-lg cursor-pointer" key={index}>
+                            {item}
+                        </h3>
+                    ))}
+                </div>
 
-                    {/*Contact */}
-                    <div className="flex flex-col gap-3 mt-6">
-                        <h3 className="text-lg md:text-xl font-bold">Contact Us</h3>
-                        {contact.map((item, index) => (
+                {/* Contact */}
+                <div className="flex flex-col gap-3 mt-6">
+                    <h3 className="text-lg md:text-xl font-bold">Contact Us</h3>
+                    {contact.map((item, index) => (
+                        <a
+                            href={item.link}
+                            className="flex items-end gap-1 flex-row leading-6 text-sm md:text-lg cursor-pointer hover:underline"
+                            key={index}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <span className="flex items-center">{item.icon}</span>
+                            {item.feature}
+                        </a>
+                    ))}
+                </div>
+
+                {/* Socials */}
+                <div className="flex flex-col gap-3 mt-6">
+                    <h3 className="text-lg md:text-xl font-bold">Socials</h3>
+                    <div className="flex flex-row lg:flex-col lg:items-center gap-3">
+                        {socials.map((item, index) => (
                             <h3
-                                className="flex items-end gap-1 flex-row leading-6 text-sm md:text-lg cursor-pointer"
+                                className="flex flex-row text-sm md:text-lg cursor-pointer"
                                 key={index}
                             >
-                                <span className="flex items-center">{item.icon}</span>
-                                {item.feature}
+                                {item}
                             </h3>
                         ))}
                     </div>
-
-                    {/*Social */}
-                    <div className="flex flex-col gap-3 mt-6">
-                        <h3 className="text-lg md:text-xl font-bold">Socials</h3>
-                        <div className="flex flex-row lg:flex-col lg:items-center gap-3">
-                            {socials.map((item, index) => (
-                                <h3
-                                    className="flex flex-row text-sm md:text-lg cursor-pointer"
-                                    key={index}
-                                >
-                                    {item}
-                                </h3>
-                            ))}
-                        </div>
-                    </div>
                 </div>
-                <h3 className="text-sm text-center mt-12 pb-2">All rights reserved. © 2024 Zeeks Solutions Limited</h3>
             </div>
-        </>
-
+            <h3 className="text-sm text-center mt-12 pb-2">
+                All rights reserved. © 2024 Zeeks Solutions Limited
+            </h3>
+        </div>
     );
 };
 
